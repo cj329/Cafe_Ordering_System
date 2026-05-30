6e75ca88e9c1f6fb97b2b34c77cd4b0a4e7dca7f
Cj Serino <mezuskie04@gmail.com>
2026-05-30 12:03:16 +0800

    Update settings.py

--- cafe_project/settings.py
+++ cafe_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%^0!myk=by4mbrw)p*6^!te%!5^0-z=%0l24mlvu$jeot^(q*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
